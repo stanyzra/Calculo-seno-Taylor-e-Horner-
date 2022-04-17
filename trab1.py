4cbc4593fa08e66c2cb4aa30448ef9e0a46bd121
stanyzra <stanyhelberth@gmail.com>
2022-04-17 06:32:17 -0300

    mudanças no método de verificação

--- trab1.py
+++ trab1.py
@@ -35,15 +35,25 @@ def verification(x):
     if (x >= 2*math.pi):
         x = x-math.floor(x/(2*math.pi))*math.pi*2
     if (x <= math.pi/4):
-        return print("sin: {:.10f} | cos: {:.10f}".format(sinCalc(x), cosCalc(x)))
+        sin = str(sinCalc(x))[:12]
+        cos = str(cosCalc(x))[:12]
+        return print("sin: {} | cos: {}".format(sin, cos))
     elif (math.pi >= x and x > math.pi/4):
-        return print("sin: {:.10f} | cos: {:.10f}".format(cosCalc(math.pi/2-x), sinCalc(math.pi/2-x)))
+        sin = str(cosCalc(math.pi/2-x))[:12]
+        cos = str(sinCalc(math.pi/2-x))[:12]
+        return print("sin: {} | cos: {}".format(sin, cos))
     elif (3*math.pi/4 >= x and x > math.pi/2):
-        return print("sin: {:.10f} | cos: {:.10f}".format(cosCalc(x-math.pi/2), sinCalc(x-math.pi/2)))     
+        sin = str(cosCalc(x-math.pi/2))[:12]
+        cos = str(sinCalc(x-math.pi/2))[:12]
+        return print("sin: {} | cos: {}".format(sin, cos))     
     elif (math.pi >= x and x > (3*math.pi)/4):
-        return print("sin: {:.10f} | cos: {:.10f}".format(sinCalc(math.pi-x), cosCalc(math.pi-x)))
+        sin = str(sinCalc(math.pi-x))[:12]
+        cos = str(cosCalc(math.pi-x))[:12]
+        return print("sin: {} | cos: {}".format(sin, cos))
     elif (x > math.pi):
-        return print("sin: {:.10f} | cos: {:.10f}".format(-1*sinCalc(x-math.pi), -cosCalc(x-math.pi)))
+        sin = str(-1*sinCalc(x-math.pi))[:12]
+        cos = str(-cosCalc(x-math.pi))[:12]
+        return print("sin: {} | cos: {}".format(sin, cos))
     
 deg = input("Insira um valor (graus): ")
 rad = float(deg)* (math.pi / 180)
